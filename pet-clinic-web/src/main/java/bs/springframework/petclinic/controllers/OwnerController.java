67e7b6b28b1aad90a470141ef9e193af05d1bb9c
Bryce Stampfl <brycestampfl@gmail.com>
2020-02-21 15:35:15 -0800

    Implemented listing Owners on the Owner Index page. Closes #17

--- pet-clinic-web/src/main/java/bs/springframework/petclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/bs/springframework/petclinic/controllers/OwnerController.java
@@ -1,15 +1,27 @@
 package bs.springframework.petclinic.controllers;
 
+import bs.springframework.petclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @RequestMapping("/owners")
 @Controller
 public class OwnerController {
 
-    @RequestMapping({"","/","/index","/index.html"})
-    public String listOwners(){
+    private final OwnerService ownerService;
 
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"","/","/index","/index.html"})
+    //Spring automatically injects Model when it calls this method
+    public String listOwners(Model model){
+        // "owners" is the name of the property/attribute inside of the model
+        model.addAttribute("owners", ownerService.findAll());
+
+        //Returns the owners index page
         return "owners/index";
     }
 }
